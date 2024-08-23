2719a65da91e59cd210114138257b07d766366d8
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2024-08-23 16:39:33 +0700

    finishing slicing bonus page

--- lib/ui/pages/bonus_page.dart
+++ lib/ui/pages/bonus_page.dart
@@ -29,6 +29,7 @@ class BonusPage extends StatelessWidget {
         ],
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
             children: [
@@ -66,10 +67,92 @@ class BonusPage extends StatelessWidget {
                     ),
                   ),
                 ),
-              )
+              ),
+              Text(
+                'Pay',
+                style: whiteTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: medium,
+                ),
+              ),
             ],
-          )
+          ),
+          SizedBox(
+            height: 41,
+          ),
+          Text(
+            'Balance',
+            style: whiteTextStyle.copyWith(
+              fontWeight: light,
+            ),
+          ),
+          Text(
+            'IDR 280.000.000',
+            style: whiteTextStyle.copyWith(
+              fontWeight: medium,
+              fontSize: 26,
+            ),
+          ),
         ],
+      ),
+    );
+  }
+
+  Widget title() {
+    return Container(
+      margin: EdgeInsets.only(
+        top: 80,
+      ),
+      child: Text(
+        'Big Bonus ',
+        style: blackTextStyle.copyWith(
+          fontSize: 32,
+          fontWeight: semiBold,
+        ),
+      ),
+    );
+  }
+
+  Widget subtitle() {
+    return Container(
+      margin: EdgeInsets.only(
+        top: 10,
+      ),
+      child: Text(
+        'We give you early credit so that\nyou can buy a flight ticket',
+        style: grayTextStyle.copyWith(
+          fontSize: 16,
+          fontWeight: light,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  Widget startButton() {
+    return Container(
+      width: 220,
+      height: 55,
+      margin: EdgeInsets.only(
+        top: 50,
+      ),
+      child: TextButton(
+        onPressed: () {},
+        style: TextButton.styleFrom(
+          backgroundColor: kPrimaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+              defaultRadius,
+            ),
+          ),
+        ),
+        child: Text(
+          'Start Fly Now',
+          style: whiteTextStyle.copyWith(
+            fontSize: 18,
+            fontWeight: medium,
+          ),
+        ),
       ),
     );
   }
@@ -83,6 +166,9 @@ class BonusPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             bonusCard(),
+            title(),
+            subtitle(),
+            startButton(),
           ],
         ),
       ),
