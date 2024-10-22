c4590c7c8f4e53d5c6e2e72c4e408238800b29c5
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2024-10-22 17:00:01 +0700

    Card Destination

---
+++ lib/ui/pages/home_page.dart
@@ -0,0 +1,113 @@
+import 'package:airplane/shared/theme.dart';
+import 'package:airplane/ui/widgets/destination_card.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+          left: defaultMargin,
+          right: defaultMargin,
+          top: 30,
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Howdy,\nKezia Anne',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: semiBold,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    'Where to fly today?',
+                    style: grayTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: light,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/profile.jpg',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget popularDestination() {
+      return Container(
+        margin: const EdgeInsets.only(
+          top: 30,
+        ),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              DestinationCard(
+                title: 'Lake Ciliwung',
+                city: 'Tangerang',
+                imageUrl: 'assets/image_destination1.png',
+                rating: 4.8,
+              ),
+              DestinationCard(
+                title: 'White Houses',
+                city: 'Spain',
+                imageUrl: 'assets/image_destination2.png',
+                rating: 5.0,
+              ),
+              DestinationCard(
+                title: 'Lake Ciliwung',
+                city: 'Monaco',
+                imageUrl: 'assets/image_destination3.png',
+                rating: 4.7,
+              ),
+              DestinationCard(
+                title: 'Lake Ciliwung',
+                city: 'Japan',
+                imageUrl: 'assets/image_destination4.png',
+                rating: 4.9,
+              ),
+              DestinationCard(
+                title: 'Lake Ciliwung',
+                city: 'Singapore',
+                imageUrl: 'assets/image_destination5.png',
+                rating: 4.5,
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return ListView(
+      children: [
+        header(),
+        popularDestination(),
+      ],
+    );
+  }
+}
