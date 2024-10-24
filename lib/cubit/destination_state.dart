54aa9251372413582f7542b60ce79834062384cd
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2024-10-24 15:27:04 +0700

    create destination cubit

---
+++ lib/cubit/destination_state.dart
@@ -0,0 +1,32 @@
+part of 'destination_cubit.dart';
+
+sealed class DestinationState extends Equatable {
+  const DestinationState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class DestinationInitial extends DestinationState {}
+
+class DestinationLoading extends DestinationState {}
+
+class DestinationSuccess extends DestinationState {
+  final List<DestinationModel> destinations;
+
+  DestinationSuccess(this.destinations);
+
+  @override
+  // TODO: implement props
+  List<Object> get props => destinations;
+}
+
+class DestinationFailed extends DestinationState {
+  final String error;
+
+  DestinationFailed(this.error);
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [error];
+}
